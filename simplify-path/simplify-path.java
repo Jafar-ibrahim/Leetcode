31a59cbc7efef2de8817f8ec4b94593ef0c0113e
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-04-14 02:17:09 +0300

    Time: 6 ms (35.26%) | Memory: 42.7 MB (53.37%) - LeetSync

--- simplify-path/simplify-path.java
+++ simplify-path/simplify-path.java
@@ -4,15 +4,17 @@ class Solution {
         Stack<String> mystack = new Stack<>();
         String[] parts = path.split("/");
         StringBuilder str = new StringBuilder();
+
         for(int i=0 ; i<parts.length ; i++){
             if(parts[i].equals("..") ){
                 if(!mystack.empty())
                     mystack.pop();
-            }else if(!Objects.equals(parts[i], "") && !Objects.equals(parts[i], ".") && !Objects.equals(parts[i], "/")){
+            }else if(!Objects.equals(parts[i], "") && !Objects.equals(parts[i], ".")){
                 mystack.push(parts[i]);
             }
         }
         if (mystack.empty()) return "/";
+        
         while (!mystack.empty()){
             str.insert(0,mystack.pop()).insert(0,"/");
         }
