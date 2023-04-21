da0d7d6c2196ca1a2a5466befec046be26221ad4
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-04-21 09:02:49 +0300

    Time: 36 ms (50.10%) | Memory: 54.8 MB (12.79%) - LeetSync

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,6 +1,28 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-       Arrays.sort(nums);
-       return nums[nums.length-k]; 
+       if(nums.length == 1 ) return nums[0];
+       
+    int pivot = nums.length-1 , pointer = 0 ;
+    while(pointer <= pivot){
+        for(int i=pointer ; i<pivot ; i++){
+            if(nums[i] <= nums[pivot]){
+                int temp = nums[pointer];
+                nums[pointer] = nums[i];
+                nums[i] = temp;
+                pointer++;
+            }
+        }
+        int temp = nums[pointer];
+        nums[pointer] = nums[pivot];
+        nums[pivot] = temp;
+        if(pointer == nums.length-k) return nums[pointer];
+        else if(pointer > nums.length-k){
+            pivot = pointer - 1;
+            pointer = 0;
+        }else{
+            pointer +=1;
+        }
+    }
+    return pointer;
     }
 }
