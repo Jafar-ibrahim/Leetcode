14a6ea416d7b6dea9c25598673584de371e20c01
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-07 14:24:39 +0300

    Time: 30 ms (98.87%) | Memory: 50.5 MB (30.89%) - LeetSync

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,29 +1,34 @@
 class Solution {
-    
-    //Runtime: 106 ms, faster than 29.54% of Java online submissions for 3Sum.
-    //Memory Usage: 70.8 MB, less than 23.87% of Java online submissions for 3Sum.
-    //HashMap
-    //Time:O(N * logN + N * N); Space: O(N + logN + N)
+    List<List<Integer>> res=new ArrayList();
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> resultSet = new HashSet();
         Arrays.sort(nums);
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++) map.put(nums[i], i);
-
-        for(int i = 0; i < nums.length - 2 && nums[i] <= 0;){
-            for (int j = i + 1; j < nums.length && nums[i] + nums[j] <= 0;) {
-                int value = 0 - nums[i] - nums[j];
-                //if (value < 0) break;
-                if (value < 0) return new ArrayList<>(resultSet);
-                if (value < nums[j]) break;
-                if (map.containsKey(value) && map.get(value) > j)
-                    resultSet.add(Arrays.asList(nums[i], nums[j], value));
-                j++;
-                while(j < nums.length && nums[j] == nums[j - 1]) j++;
+        for(int i=0;i<nums.length;i++){
+            if(i==0 || nums[i-1]!=nums[i]){
+                 twoSum(i+1,nums.length-1,nums,0-nums[i]);
             }
-            i++;
-            while(i < nums.length - 2 && nums[i] == nums[i - 1]) i++;
+          
         }
-        return new ArrayList<>(resultSet) ;
+        return res;
+    }
+    void twoSum(int i,int j,int[] nums,int target){
+    int a=nums[i-1];
+    while(i<j){
+        if(nums[i]+nums[j]>target){
+            j--;
+        }
+         else if(nums[i]+nums[j]<target){
+            i++;
+        }
+        else{
+            List<Integer> list=new ArrayList();
+            list.add(a);list.add(nums[i]);list.add(nums[j]);
+            res.add(list);
+            while(i<j && nums[i]==nums[i+1]) i++;
+            while(i<j && nums[j]==nums[j-1]) j--;;
+            i++;
+            j--;
+
+        }
+    }
     }
 }
