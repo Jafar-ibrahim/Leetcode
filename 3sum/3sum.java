9db661e4e43eca9f1d3c8cef486f469d95cef9a2
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-07 14:13:29 +0300

    Time: 78 ms (42.27%) | Memory: 50.9 MB (8.45%) - LeetSync

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,26 +1,29 @@
 class Solution {
+    
+    //Runtime: 106 ms, faster than 29.54% of Java online submissions for 3Sum.
+    //Memory Usage: 70.8 MB, less than 23.87% of Java online submissions for 3Sum.
+    //HashMap
+    //Time:O(N * logN + N * N); Space: O(N + logN + N)
     public List<List<Integer>> threeSum(int[] nums) {
-        int target = 0;
+        Set<List<Integer>> resultSet = new HashSet();
         Arrays.sort(nums);
-        Set<List<Integer>> s = new HashSet<>();
-        List<List<Integer>> output = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++){
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                } else if (sum < target) {
-                    j++;
-                } else {
-                    k--;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) map.put(nums[i], i);
+
+        for(int i = 0; i < nums.length - 2 && nums[i] <= 0;){
+            for (int j = i + 1; j < nums.length && nums[i] + nums[j] <= 0;) {
+                int value = 0 - nums[i] - nums[j];
+                //if (value < 0) break;
+                if (value < 0) return new ArrayList<>(resultSet);
+                if (value < nums[j]) break;
+                if (map.containsKey(value) && map.get(value) > j)
+                    resultSet.add(Arrays.asList(nums[i], nums[j], value));
+                j++;
+                while(j < nums.length && nums[j] == nums[j - 1]) j++;
             }
+            i++;
+            while(i < nums.length - 2 && nums[i] == nums[i - 1]) i++;
         }
-        output.addAll(s);
-        return output;
+        return new ArrayList<>(resultSet) ;
     }
 }
