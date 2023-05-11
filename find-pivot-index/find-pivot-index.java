1fc89a534de40390904af7cb0412d0d2c15a04b6
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-11 13:07:32 +0300

    Time: 1 ms (100.00%) | Memory: 43.3 MB (47.63%) - LeetSync

--- find-pivot-index/find-pivot-index.java
+++ find-pivot-index/find-pivot-index.java
@@ -1,12 +1,16 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        // make a prefix sum array with one more element to store 0 as the fist value
         int[] cumulative  = new int[nums.length+1];
         cumulative[0]=0;
         int sum=0;
+        // fill prefix sum array
         for(int i=1;i<=nums.length;i++){
             cumulative[i] = cumulative[i-1] + nums[i-1];
         }
+        // subtract the current sum from the greatest sum until the result equals the sum before the currnt index  
        for(int i=1;i<cumulative.length;i++){
+           // is equal , return the index subtracted by one to align with the original array's index 
             if(cumulative[cumulative.length-1] - cumulative[i] == cumulative[i-1]) return i-1;
         }
         return -1;
