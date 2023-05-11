f8d4cd2718bcac42901cad304e059db540db77d1
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-11 16:08:16 +0300

    Time: 0 ms (100.00%) | Memory: 44 MB (22.27%) - LeetSync

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -11,8 +11,10 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        // if there is only 1 or 0 nodes , its not possible to have a cycle
         if (head==null || head.next==null) return false;
         ListNode fast = head.next , slow = head;
+        // keep running two pointers until they meet or the list ends
         while(fast!=null && fast.next!=null && slow != fast){
             slow = slow.next;
             fast=fast.next.next;
