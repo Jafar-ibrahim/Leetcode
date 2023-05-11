99ba3521e01f7156960a4871065200c4b85d6b89
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-11 17:54:25 +0300

    Time: 0 ms (100.00%) | Memory: 42.4 MB (28.81%) - LeetSync

--- merge-two-sorted-lists/merge-two-sorted-lists.java
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,8 +10,6 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode i = list1;
-        ListNode j = list2;
         ListNode answer = new ListNode(-1);
         ListNode k =answer;
         while(list1 != null && list2!= null){
@@ -25,15 +23,11 @@ class Solution {
                 list1 = list1.next;
             }
         }
-        while(list1 != null){
-            k.next = new ListNode(list1.val);
-            k=k.next;
-            list1 = list1.next;
+        if(list1 != null){
+            k.next = list1;
         }
-        while(list2 != null){
-            k.next = new ListNode(list2.val);
-            k=k.next;
-            list2 = list2.next;
+        if(list2 != null){
+            k.next = list2;
         }
         return answer.next;
     }
