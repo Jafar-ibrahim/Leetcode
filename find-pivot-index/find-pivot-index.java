069a72cc7c3d8333a23daaac7cb18e7883a8395a
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-11 13:03:16 +0300

    Time: 1 ms (100.00%) | Memory: 43.2 MB (60.08%) - LeetSync

--- find-pivot-index/find-pivot-index.java
+++ find-pivot-index/find-pivot-index.java
@@ -1,15 +1,13 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int[] cumulative  = new int[nums.length];
+        int[] cumulative  = new int[nums.length+1];
+        cumulative[0]=0;
         int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-            cumulative[i] = sum;
+        for(int i=1;i<=nums.length;i++){
+            cumulative[i] = cumulative[i-1] + nums[i-1];
         }
-       int prev = 0 , max=cumulative[cumulative.length-1];
-       for(int i=0;i<cumulative.length;i++){
-            if(max - cumulative[i] == prev) return i;
-            prev = cumulative[i];
+       for(int i=1;i<cumulative.length;i++){
+            if(cumulative[cumulative.length-1] - cumulative[i] == cumulative[i-1]) return i-1;
         }
         return -1;
     }
