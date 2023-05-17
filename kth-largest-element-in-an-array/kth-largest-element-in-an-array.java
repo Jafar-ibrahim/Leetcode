bb70b4de2f569c9aec26772d20c3644c27e7eac2
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-18 00:42:42 +0300

    Time: 34 ms (52.27%) | Memory: 54.5 MB (13.87%) - LeetSync

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,12 +1,30 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for (int num : nums) {
-            minHeap.add(num);
-            if (minHeap.size() > k) {
-                minHeap.poll();
+        int target = nums.length-k;
+        int pivot=0,end=nums.length-1;
+        while(pivot <= end){
+          int start = pivot;
+          for(int i =pivot ; i<end ; i++){
+            if(nums[i] <= nums[end]){
+              swap(pivot,i,nums);
+              pivot++;
             }
+          }
+          swap(end,pivot,nums);
+          if(target == pivot) return nums[pivot];
+          else if(target >  pivot){
+            pivot++;
+          }else{
+            end=pivot-1;
+            pivot=start;
+          }
         }
-        return minHeap.peek();
+        return -1;
+
+    }
+    public static void swap(int index1,int index2,int[] nums){
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
 }
