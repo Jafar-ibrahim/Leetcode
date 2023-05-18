5c97d71ea26b2ddefb847e2f4901a194106559ab
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-19 01:50:38 +0300

    Time: 86 ms (12.51%) | Memory: 47 MB (100.00%) - LeetSync

--- k-closest-points-to-origin/k-closest-points-to-origin.java
+++ k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         int[][] answer = new int[k][2];
-        PriorityQueue<int[]> max_heap = new PriorityQueue<>((x,y)-> difference(distance(y),distance(x)));
+        PriorityQueue<int[]> max_heap = new PriorityQueue<>((x,y)-> Double.compare(distance(y),distance(x)));
+        
         for (int[] point : points) {
             max_heap.offer(point);
             if(max_heap.size() > k)
@@ -18,10 +19,5 @@ class Solution {
     public static double distance(int[] point){
         return (Math.pow(Math.pow(point[0],2) + Math.pow(point[1],2) ,0.5));
     }
-    public static int difference(double x , double y){
-        double diff = x - y;
-        if(diff > 0) return 1;
-        else if(diff <0) return -1;
-        else return 0;
-    }
+    
 }
