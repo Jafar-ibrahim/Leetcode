b8bbdc0a52b39074d46763659d0385b4c1625700
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-20 01:20:55 +0300

    Time: 4 ms (97.35%) | Memory: 45.1 MB (8.79%) - LeetSync

--- find-k-closest-elements/find-k-closest-elements.java
+++ find-k-closest-elements/find-k-closest-elements.java
@@ -1,44 +1,18 @@
 class Solution {
-
-class Pair{
-    int key;
-    int value;
-    
-    Pair(int key, int value){
-        this.key = key;
-        this.value = value;
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int lo = 0;
+		int hi = arr.length - 1;
+		while (hi - lo >= k) {
+			if (Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)) {
+				lo++;
+			} else {
+				hi--;
+			}
+		}
+		List<Integer> result = new ArrayList<>(k);
+		for (int i = lo; i <= hi; i++) {
+			result.add(arr[i]);
+		}
+		return result;
     }
-    
-}
-
-public List<Integer> findClosestElements(int[] arr, int k, int x) {
-    int n = arr.length;
-    List<Integer> list = new ArrayList<>();
-    
-    // make max priority queue
-    PriorityQueue<Pair> pq = new PriorityQueue<>(
-        new Comparator<Pair>(){
-            public int compare(Pair a, Pair b){
-                // if both the keys are same then compare on the bases of their values
-                if((b.key - a.key) == 0)
-                    return b.value - a.value;
-                return b.key - a.key;
-            }
-    });
-    
-    for(int i: arr){
-        pq.add(new Pair(Math.abs(i-x), i));
-        
-        if(pq.size() > k)
-            pq.poll();
-    }
-    
-    while(pq.size() != 0){
-        Pair p = pq.poll();
-        list.add(p.value);
-    }
-    
-    Collections.sort(list);
-    return list;
-}
 }
