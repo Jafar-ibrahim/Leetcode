b02012a3114d7a07107cca8eef6f3eba71427bfc
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-21 23:58:18 +0300

    Time: 8 ms (45.36%) | Memory: 46.5 MB (29.68%) - LeetSync

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,19 +1,14 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs){
-        List<List<String>>s=new ArrayList<>();
-        Map<String,List<String>>map=new LinkedHashMap<>();
-        
-        for(String e:strs){
-            char ch[]=e.toCharArray();
-            Arrays.sort(ch);
-            String str=new String(ch);
-            
-            if(!map.containsKey(str)){
-                map.put(str,new ArrayList<>());
-            }
-            map.get(str).add(e);
+        public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = new char[26];
+            for (char c : s.toCharArray()) ca[c - 'a']++;
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
         }
-        s.addAll(map.values());
-        return s;
+        return new ArrayList<>(map.values());
     }
 }
