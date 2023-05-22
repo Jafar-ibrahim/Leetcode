255f73f2c62046397c6a95fd44c7ca69b7d0188e
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-22 23:15:24 +0300

    Time: 29 ms (84.56%) | Memory: 44.1 MB (33.49%) - LeetSync

--- hand-of-straights/hand-of-straights.java
+++ hand-of-straights/hand-of-straights.java
@@ -1,14 +1,22 @@
 class Solution {
-    public boolean isNStraightHand(int[] hand, int W) {
-        if(hand.length % W != 0) return false;
-
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(int elem: hand) minHeap.add(elem);
-
-        while(!minHeap.isEmpty()){
-            int head = minHeap.poll();
-            for(int i=1; i<W; i++)
-                if(!minHeap.remove(head+i)) return false;
+    public boolean isNStraightHand(int[] hand, int g) {
+        Arrays.sort(hand);
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        for(int i:hand)hm.put(i,hm.getOrDefault(i,0)+1);
+        
+        for(int i:hand){
+            if(hm.get(i)>=1){
+                hm.put(i,hm.get(i)-1);
+                 int size = 1;
+                int mem = i+1;
+                while(size<g){
+                    if(!hm.containsKey(mem) || hm.get(mem)<=0)return false;
+                    hm.put(mem,hm.get(mem)-1);
+                    mem++;
+                    size++;
+                }
+            }
         }
         return true;
     }
