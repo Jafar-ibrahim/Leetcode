8580aa836edafb54273b69ac494629a299fa9a4a
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-26 00:28:28 +0300

    Time: 1 ms (89.57%) | Memory: 41.1 MB (94.28%) - LeetSync

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,26 +1,48 @@
-public class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
-    {
-        List<List<Integer>> sol = new ArrayList<>();
-        travel(root, sol, 0);
-        return sol;
-    }
-    
-    private void travel(TreeNode curr, List<List<Integer>> sol, int level)
-    {
-        if(curr == null) return;
-        
-        if(sol.size() <= level)
-        {
-            List<Integer> newLevel = new LinkedList<>();
-            sol.add(newLevel);
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> answer = new ArrayList<>();
+        if(root == null) return answer;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean reversed = false;
+        int level =0;
+        while(!queue.isEmpty()){
+            List<Integer> temp = new ArrayList<>();
+            int size =queue.size(); 
+            for(int i=0 ; i<size;i++){
+                TreeNode curr = queue.remove();
+                if(curr != null){
+                    if(level % 2 == 0)
+                        temp.add(curr.val);
+                    else
+                        temp.add(0,curr.val);
+                    
+                    queue.add(curr.left);
+                    queue.add(curr.right);
+                }
+            }
+            if(!temp.isEmpty()){
+                answer.add(temp);
+            }
+            level++;
         }
-        
-        List<Integer> collection  = sol.get(level);
-        if(level % 2 == 0) collection.add(curr.val);
-        else collection.add(0, curr.val);
-        
-        travel(curr.left, sol, level + 1);
-        travel(curr.right, sol, level + 1);
+
+        return answer;
     }
 }
