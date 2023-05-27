e97e005939ca3d8ee2e8da71115181ebca94949b
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-05-27 13:00:35 +0300

    Time: 5 ms (99.37%) | Memory: 44 MB (5.78%) - LeetSync

--- top-k-frequent-words/top-k-frequent-words.java
+++ top-k-frequent-words/top-k-frequent-words.java
@@ -1,36 +1,26 @@
 class Solution {
-    class Pair{
-        String word;
-        int freq;
-        Pair(String word, int freq){
-            this.word=word;
-            this.freq=freq;
+    
+    public static List<String> topKFrequent(String[] words, int k) {
+        PriorityQueue<Map.Entry<String,Integer>> max_heap =
+                new PriorityQueue<>(new Comparator<Map.Entry<String,Integer>>(){
+                    public int compare(Map.Entry<String,Integer> a, Map.Entry<String,Integer> b){
+                        if(a.getValue()==b.getValue())
+                            return a.getKey().compareTo(b.getKey());
+                        else
+                            return b.getValue()-a.getValue();
+                    }
+                });
+        Map<String,Integer> map = new HashMap<>();
+        for (String s : words){
+            map.put(s,map.getOrDefault(s,0)+1);
         }
-    }
-    public List<String> topKFrequent(String[] words, int k) {
-        HashMap<String, Integer>map=new HashMap<>();
-        for(String w:words){
-            map.put(w,map.getOrDefault(w,0)+1);
+        max_heap.addAll(map.entrySet());
+        List<String> answer = new ArrayList<>();
+        for (int i=0 ; i<k ;i++){
+            answer.add(max_heap.poll().getKey());
         }
-        PriorityQueue<Pair> maxheap=new PriorityQueue<>(new Comparator<Pair>(){
-            public int compare(Pair a, Pair b){
-                if(a.freq==b.freq)
-                    return a.word.compareTo(b.word);
-                else 
-                    return b.freq-a.freq;
-            }
-        });
-      
 
-        for(Map.Entry<String, Integer>entry:map.entrySet()){
-            maxheap.add(new Pair(entry.getKey(),entry.getValue()));
-        }
-            List<String> res = new ArrayList<>();
-            for(int i=0;i<k;i++){
-                Pair rem=maxheap.poll();
-                res.add(rem.word);
-            }
-        return res;
-        }
+        return answer;
+    }
         
     }
