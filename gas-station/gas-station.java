7e0481fc4258e0680533859b5111a950f753f286
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-06-10 16:20:18 +0300

    Time: 3 ms (36.40%) | Memory: 61 MB (8.86%) - LeetSync

--- gas-station/gas-station.java
+++ gas-station/gas-station.java
@@ -4,22 +4,16 @@ class Solution {
         for(int i=0 ; i< diff.length ; i++){
             diff[i] = gas[i] - cost[i];
         }
-        int curr = 0 , index = 0 ;
+        int curr = 0 , total =0,index = 0 ;
         for(int i=0 ; i< diff.length ; i++){
             curr+= diff[i];
+            total += diff[i];
             if(curr < 0) {
                 curr = 0;
                 index = i+1;
-                continue;
             }
         }
-        if(curr == 0 && index > 0) return -1;
-        for(int i=0 ; i< index ; i++){
-            curr += diff[i];
-            if(curr < 0) {
-                return -1;
-            }
-        }
-        return index;
+        
+        return (total < 0)? -1 : index;
     }
 }
