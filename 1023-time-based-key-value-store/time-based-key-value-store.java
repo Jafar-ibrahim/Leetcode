76b17c559e884b671471d6315bc1d70c3de36b56
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-07-25 22:23:08 +0300

    Time: 168 ms (43.69%) | Memory: 118.7 MB (70.70%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,44 +1,31 @@
- class subMap{
-        TreeMap<Integer,String> innerMap ;
-        int lastAdded;
+class TimeMap {
+    private static final String DEFAULT_VALUE = "";
+    private final HashMap<String, TreeMap<Integer, String>> map;
 
-        public subMap(){
-            innerMap = new TreeMap<>();
-            lastAdded = 0;
-        }
+    /** Initialize your data structure here. */
+    public TimeMap() {
+        map = new HashMap<>();
     }
-
-     class TimeMap {
-
-        Map<String,subMap> map ;
-
-        public TimeMap() {
-            map = new HashMap<>();
+    
+    public void set(String key, String value, int timestamp) {
+        TreeMap<Integer, String> timeMap;
+        if (map.containsKey(key)) {
+            timeMap = map.get(key);
+        } else {
+            timeMap = new TreeMap<>();
+            map.put(key, timeMap);
         }
-
-        public void set(String key, String value, int timestamp) {
-            if(map.get(key) == null){
-                map.put(key,new subMap());
-            }
-            map.get(key).innerMap.put(timestamp,value);
-            if(map.get(key).lastAdded < timestamp)
-                map.get(key).lastAdded = timestamp;
-        }
-
-        public String get(String key, int timestamp) {
-            if(map.get(key) != null) {
-                TreeMap<Integer, String> currMap = map.get(key).innerMap;
-                if (currMap.get(timestamp) != null)
-                    return currMap.get(timestamp);
-                if (timestamp >= map.get(key).lastAdded)
-                    return currMap.get(map.get(key).lastAdded);
-
-                Integer floorKey = currMap.floorKey(timestamp);
-                if (floorKey != null) {
-                    return currMap.get(floorKey);
-                }
-            }
-
-            return "";
-        }
+        timeMap.put(timestamp, value);
     }
+    
+    public String get(String key, int timestamp) {
+        if (map.containsKey(key)) {
+            TreeMap<Integer, String> timeMap = map.get(key);
+            Integer floorKey = timeMap.floorKey(timestamp);
+            if (floorKey != null) {
+                return timeMap.get(floorKey);
+            }
+        }
+        return DEFAULT_VALUE;
+    }
+}
