f88d37d575e57ae896b22f2b57877c1f373d507c
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-07-27 18:25:28 +0300

    Time: 8 ms (97.21%) | Memory: 45.3 MB (85.98%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, Comparator.comparingInt(x -> x[0]));
-        ArrayList<int[]> answer = new ArrayList<>();
-        int length=0;
-        int currEnd = intervals[0][1];
-        int currStart = intervals[0][0];
-        for(int i=1 ; i< intervals.length  ; i++){
-            if( intervals[i][0] > currEnd) {
-                answer.add(new int[]{currStart,currEnd});
-                currStart = intervals[i][0];
-                length++;
-            }
-            currEnd = Math.max(currEnd,intervals[i][1]);
-        }
-            answer.add(new int[]{currStart,currEnd});
-            return answer.toArray(new int[length][]);
-    }
+	public int[][] merge(int[][] intervals) {
+		if (intervals.length <= 1)
+			return intervals;
+
+		// Sort by ascending starting point
+		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+		List<int[]> result = new ArrayList<>();
+		int[] newInterval = intervals[0];
+		result.add(newInterval);
+		for (int[] interval : intervals) {
+			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
+				newInterval[1] = Math.max(newInterval[1], interval[1]);
+			else {                             // Disjoint intervals, add the new interval to the list
+				newInterval = interval;
+				result.add(newInterval);
+			}
+		}
+
+		return result.toArray(new int[result.size()][]);
+	}
 }
