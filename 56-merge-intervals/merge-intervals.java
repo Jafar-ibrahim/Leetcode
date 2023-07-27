bbb7cbcb31030255ff99388cda235ae36641f18c
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-07-27 18:21:15 +0300

    Time: 20 ms (5.98%) | Memory: 47.2 MB (5.28%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(x -> x[0]));
+        System.out.println(Arrays.deepToString(intervals));
+        ArrayList<int[]> answer = new ArrayList<>();
+        int length=0;
+        int currEnd = intervals[0][1];
+        int currStart = intervals[0][0];
+        for(int i=1 ; i< intervals.length  ; i++){
+            if( intervals[i][0] > currEnd) {
+                answer.add(new int[]{currStart,currEnd});
+                currStart = intervals[i][0];
+                length++;
+            }
+            currEnd = Math.max(currEnd,intervals[i][1]);
+            //currStart = intervals[i][0];
+        }
+            answer.add(new int[]{currStart,currEnd});
+            return answer.toArray(new int[length][]);
+    }
+}
