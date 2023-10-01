662b5c56b3efce4d2a5a7bcd1c43383d98aaf835
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-02 01:41:13 +0300

    Time: 1 ms (99.69%) | Memory: 40.9 MB (97.27%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.java
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -1,24 +1,47 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
-    String str;
-    public List<String> restoreIpAddresses(String s) {
-        str = s;
-        magical("", 0, 0);
-        return ans;
-    } 
-
-    void magical( String path, int index, int dots) {
-        if (dots > 4) return;
-        if (dots == 4 && index >= str.length()) {
-            ans.add(path.substring(0,path.length()-1));
+    private boolean valid(String s, int start, int length) {
+        return length == 1 || 
+            (s.charAt(start) != '0' && 
+             (length < 3 || 
+              s.substring(start, start + length).compareTo("255") <= 0));
+    }
+    
+    private void helper(String s, int startIndex, List<Integer> dots, List<String> ans) {
+        final int remainingLength = s.length() - startIndex;
+        final int remainingNumberOfIntegers = 4 - dots.size();
+        if (remainingLength > remainingNumberOfIntegers * 3 || 
+            remainingLength < remainingNumberOfIntegers) {
             return;
         }
-        for (int length = 1; length <= 3 && index + length <= str.length(); length++) {
-            String num = str.substring(index, index + length);
-            if (num.charAt(0) == '0' && length != 1) break;
-            else if (Integer.parseInt(num) <= 255) {
-                magical( path + str.substring(index, index + length) + ".", index + length,dots + 1);
+        if (dots.size() == 3) {
+            if (valid(s, startIndex, remainingLength)) {
+                StringBuilder sb = new StringBuilder();
+                int last = 0;
+                for (Integer dot : dots) {
+                    sb.append(s.substring(last, last + dot));
+                    last += dot;
+                    sb.append('.');
+                }
+                sb.append(s.substring(startIndex));
+                ans.add(sb.toString());
             }
+            return;
         }
+        for (int curPos = 1; curPos <= 3 && curPos <= remainingLength; ++curPos) {
+            // Append a dot at the current position.
+            dots.add(curPos);
+            // Try making all combinations with the remaining string.
+            if (valid(s, startIndex, curPos)) {
+                helper(s, startIndex + curPos, dots, ans);
+            }
+            // Backtrack, i.e. remove the dot to try placing it at the next position.
+            dots.remove(dots.size() - 1);
+        }
+    }
+    
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+        helper(s, 0, new ArrayList<>(), ans);
+        return ans;   
     }
 }
