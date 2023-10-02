089dd7bd51c6f3a20f151db61bd1c0e3af3eeb9e
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-02 23:54:22 +0300

    Time: 10 ms (93.70%) | Memory: 92.3 MB (40.89%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,14 +1,12 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> answer = new ArrayList<>();
-        boolean[] visited = new boolean[n+1];
         backtrack(n ,k,new ArrayList<Integer>(), answer ,1 );
         return answer;
     }
 
     public static void backtrack(int n ,int k , ArrayList<Integer> curr, List<List<Integer>> answer , int index){
 
-        // 1 2 3 4 5 6
         if(curr.size() == k){
             answer.add(new ArrayList<>(curr));
             return;
@@ -16,9 +14,7 @@ class Solution {
 
         for(int i=index ; i <= n ; i++){
             curr.add(i);
-            //visited[i] = true;
-            backtrack(n,k, curr , answer , i+1 );
-            //visited[i] = false;
+            backtrack(n , k, curr , answer , i+1 );
             curr.remove(curr.size()-1);
         }
     }
