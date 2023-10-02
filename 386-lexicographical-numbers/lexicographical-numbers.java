4e84ed0ed34c7219e28a55d0187e9e5e15962197
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-02 20:22:55 +0300

    Time: 1 ms (100.00%) | Memory: 47.4 MB (42.47%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -2,10 +2,6 @@ class Solution {
 
     public List<Integer> lexicalOrder(int n) {
         List<Integer> answer = new ArrayList<>();
-        /*for(int i = 1 ; i <= Math.min(n,9) ; i++){
-            answer.add(i);
-            generate(i*10 ,i*10 + 10, n , answer);
-        }*/
         generate(1 ,9 , n , answer);
         return answer;
     }
@@ -17,14 +13,8 @@ class Solution {
         answer.add(curr );
         int pow = 0;
         int temp = curr;
-        /*int nextBound = curr * (int) Math.pow(10,pow);*/
         
-        /*while(temp > 10){
-            temp /= 10;
-            pow++;
-        }*/
-        
-        generate(curr*10 , curr*10 +9 /*nextBound+(int)Math.pow(10,pow)-1*/ , n , answer);
+        generate(curr*10 , curr*10 +9 , n , answer);
         generate(curr + 1 , max ,  n , answer);
     }
 }
