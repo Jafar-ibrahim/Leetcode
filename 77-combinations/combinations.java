a03427f7b2ff0617250280d723551f8754ec6bd2
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-03 00:12:38 +0300

    Time: 8 ms (98.64%) | Memory: 92.6 MB (27.05%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> answer = new ArrayList<>();
-        backtrack(n ,k,new ArrayList<Integer>(), answer ,1 );
+        backtrack(new ArrayList<Integer>(), answer ,1,n,k );
         return answer;
     }
 
-    public static void backtrack(int n ,int k , ArrayList<Integer> curr, List<List<Integer>> answer , int index){
+    public static void backtrack( ArrayList<Integer> curr, List<List<Integer>> answer , int index, int n ,int k ){
 
-        if(curr.size() == k){
+        if(k== 0){
             answer.add(new ArrayList<>(curr));
             return;
         }
 
-        for(int i=index ; i <= n ; i++){
+        for(int i=index ; i <= n-k + 1 ; i++){
             curr.add(i);
-            backtrack(n , k, curr , answer , i+1 );
+            backtrack(curr , answer , i+1 ,n,k-1);
             curr.remove(curr.size()-1);
         }
     }
