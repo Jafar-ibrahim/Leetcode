2beced0da40b6d2a522486b1ea2b7228640a7aca
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-04 00:36:55 +0300

    Time: 1 ms (99.92%) | Memory: 44.2 MB (11.44%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,23 +1,20 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
         Arrays.sort(nums);
-        backtrack(nums, answer ,new ArrayList<>(), visited ,0);
+        backtrack(nums, answer ,new ArrayList<>(),0);
         return answer;
     }
 
-    public static void backtrack(int[] nums ,  List<List<Integer>> answer,ArrayList<Integer> curr, boolean[] visited , int index ){
+    public static void backtrack(int[] nums ,  List<List<Integer>> answer,ArrayList<Integer> curr, int index ){
 
         answer.add(new ArrayList(curr));
 
         for(int i =index ; i<nums.length ; i++){
-            if(visited[i] ||i > 0 && nums[i-1] == nums[i] && !visited[i-1])continue;
-            visited[i] = true;
+            if(i > index && nums[i-1] == nums[i] )continue;
             curr.add(nums[i]);
-            backtrack(nums , answer ,curr , visited , i +1);
+            backtrack(nums , answer ,curr , i +1);
             curr.remove(curr.size()-1);
-            visited[i] = false;
         }
     }
 }
