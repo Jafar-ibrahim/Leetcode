5c57ce99e9f22f82a6b941476439ad484180f5ee
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-08 21:25:30 +0300

    Time: 1 ms (99.99%) | Memory: 43.4 MB (95.63%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,20 +1,24 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-}
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{ 
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-            tempList.remove(tempList.size() - 1);
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, answer , new ArrayList() , target , 0,0);
+        return answer;
+    }
+
+    public static void backtrack(int[] candidates , List<List<Integer>> answer , ArrayList<Integer> curr , int target , int index , int sum){
+
+        if(sum == target){
+            answer.add(new ArrayList(curr));
+            return;
+        }
+
+        for(int i = index ; i < candidates.length && candidates[i]<=target; i++){
+            if(sum+candidates[i] > target) break;
+            curr.add(candidates[i]);
+            backtrack(candidates,answer,curr,target,i,sum+candidates[i]);
+            curr.remove(curr.size() - 1);
         }
     }
-}
 }
