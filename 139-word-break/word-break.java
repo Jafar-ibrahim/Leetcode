a4eeb13d7a5d371edc63f6605648d1133845a606
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-10 20:30:43 +0300

    Time: 9 ms (22.59%) | Memory: 43.2 MB (56.98%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,115 +1,34 @@
-class TrieNode {
-
-    // R links to node children
-    private TrieNode[] links;
-
-    private final int R = 26;
-
-    private boolean isEnd;
-
-    public TrieNode() {
-        links = new TrieNode[R];
-    }
-
-    public boolean containsKey(char ch) {
-        return links[ch -'a'] != null;
-    }
-    public TrieNode get(char ch) {
-        return links[ch -'a'];
-    }
-    public void put(char ch, TrieNode node) {
-        links[ch -'a'] = node;
-    }
-    public void setEnd() {
-        isEnd = true;
-    }
-    public boolean isEnd() {
-        return isEnd;
-    }
-}
-    class Trie {
-    private TrieNode root;
-
-    public Trie() {
-        root = new TrieNode();
-    }
-
-    // Inserts a word into the trie.
-    public void insert(String word) {
-        TrieNode node = root;
-        for (int i = 0; i < word.length(); i++) {
-            char currentChar = word.charAt(i);
-            if (!node.containsKey(currentChar)) {
-                node.put(currentChar, new TrieNode());
+public class Solution {
+    public boolean wordBreak(String s, List<String> dict) {
+        
+        boolean[] f = new boolean[s.length() + 1];
+        
+        f[0] = true;
+        
+        /* First DP
+        for(int i = 1; i <= s.length(); i++){
+            for(String str: dict){
+                if(str.length() <= i){
+                    if(f[i - str.length()]){
+                        if(s.substring(i-str.length(), i).equals(str)){
+                            f[i] = true;
+                            break;
+                        }
+                    }
+                }
             }
-            node = node.get(currentChar);
-        }
-        node.setEnd();
-    }
-
-    private TrieNode searchPrefix(String word) {
-        TrieNode node = root;
-        for (int i = 0; i < word.length(); i++) {
-           char curLetter = word.charAt(i);
-           if (node.containsKey(curLetter)) {
-               node = node.get(curLetter);
-           } else {
-               return null;
-           }
-        }
-        return node;
-    }
-
-    // Returns if the word is in the trie.
-    public boolean search(String word) {
-       TrieNode node = searchPrefix(word);
-       return node != null && node.isEnd();
-    }
-
-    public boolean startsWith(String prefix) {
-        TrieNode node = searchPrefix(prefix);
-        return node != null;
-    }
-
-    public TrieNode getRoot() {
-        return root;
-    }
-
-
-}
-
-class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Trie trie = new Trie();
-        for (String st : wordDict) {
-            trie.insert(st);
-        }
-        return backtrack(s, trie, 0, new Boolean[s.length()]);
-    }
-
-    public boolean backtrack(String s, Trie trie, int index, Boolean[] memo) {
-        if (index == s.length()) {
-            return true;
-        }
-
-        if (memo[index] != null) {
-            return memo[index];
-        }
-
-        TrieNode node = trie.getRoot();
-        for (int i = index; i < s.length(); i++) {
-            char curChar = s.charAt(i);
-            if (!node.containsKey(curChar)) {
-                break;
-            }
-            node = node.get(curChar);
-            if (node.isEnd() && backtrack(s, trie, i + 1, memo)) {
-                memo[index] = true;
-                return true;
+        }*/
+        
+        //Second DP
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && dict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
+                }
             }
         }
-
-        memo[index] = false;
-        return false;
+        
+        return f[s.length()];
     }
 }
