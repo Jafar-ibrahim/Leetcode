7e55657eafd5dcdcf4581e694ba4045c29a6fb45
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-17 18:54:08 +0300

    Time: 126 ms (77.91%) | Memory: 44 MB (68.80%) - LeetSync

--- 212-word-search-ii/word-search-ii.java
+++ 212-word-search-ii/word-search-ii.java
@@ -1,49 +1,68 @@
-class TrieNode {
-    TrieNode[] next = new TrieNode[26];
+class TrieNode{
+    TrieNode[] children = new TrieNode[26];;
     String word;
 }
 
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-    List<String> res = new ArrayList<>();
-    TrieNode root = buildTrie(words);
-    for (int i = 0; i < board.length; i++) {
-        for (int j = 0; j < board[0].length; j++) {
-            dfs (board, i, j, root, res);
+        
+        List<String> answer = new ArrayList();
+        TrieNode trie = new TrieNode();
+        buildTrie(trie , words);
+
+
+        for(int i = 0 ; i < board.length ; i++){
+            for(int j = 0 ; j < board[i].length ; j++){
+                
+                    backtrack(board,words,trie,answer,i,j);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void backtrack(char[][] board, String[] words,TrieNode trie,List<String> answer , int row , int col){
+
+        char c = board[row][col];
+
+if(c == '#' || trie.children[c-'a'] == null)
+            return;
+
+        trie = trie.children[c-'a'];
+        if(trie.word != null ){
+            answer.add(trie.word);
+            trie.word = null;
+        }
+        
+
+        
+ 
+        board[row][col] = '#';
+        if(row-1 >= 0) 
+            backtrack(board,words,trie,answer,row-1,col);
+        if(col-1 >= 0) 
+            backtrack(board,words,trie,answer,row,col-1);
+        if(row+1 < board.length) 
+            backtrack(board,words,trie,answer,row+1,col);
+        if(col+1 < board[0].length) 
+            backtrack(board,words,trie,answer,row,col+1);
+        
+        board[row][col] = c;
+    }
+
+    public static void buildTrie(TrieNode root , String[] words){
+
+        for(String word : words){
+            TrieNode curr = root;
+            char[] wordChars = word.toCharArray();
+            for(int i = 0 ; i < wordChars.length ; i++){
+                int index = wordChars[i] - 'a';
+                
+                if(curr.children[index] == null)
+                    curr.children[index] = new TrieNode();
+                curr = curr.children[index];
+            }
+            curr.word = word; 
         }
     }
-    return res;
-}
-
-public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
-    char c = board[i][j];
-    if (c == '#' || p.next[c - 'a'] == null) return;
-    p = p.next[c - 'a'];
-    if (p.word != null) {   // found one
-        res.add(p.word);
-        p.word = null;     // de-duplicate
-    }
-
-    board[i][j] = '#';
-    if (i > 0) dfs(board, i - 1, j ,p, res); 
-    if (j > 0) dfs(board, i, j - 1, p, res);
-    if (i < board.length - 1) dfs(board, i + 1, j, p, res); 
-    if (j < board[0].length - 1) dfs(board, i, j + 1, p, res); 
-    board[i][j] = c;
-}
-
-public TrieNode buildTrie(String[] words) {
-    TrieNode root = new TrieNode();
-    for (String w : words) {
-        TrieNode p = root;
-        for (char c : w.toCharArray()) {
-            int i = c - 'a';
-            if (p.next[i] == null) p.next[i] = new TrieNode();
-            p = p.next[i];
-       }
-       p.word = w;
-    }
-    return root;
-}
-
 }
