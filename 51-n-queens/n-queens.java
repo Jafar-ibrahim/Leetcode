a89a0288b476e5b923c538c62250d2ebb6f04b0b
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-21 23:15:45 +0300

    Time: 1 ms (100.00%) | Memory: 44 MB (37.45%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -12,11 +12,10 @@ class Solution {
             }
         }
 
-        boolean[][] bool = new boolean[4][];
+        boolean[][] bool = new boolean[3][];
         bool[0] = new boolean[n];
-        bool[1] = new boolean[n];
+        bool[1] = new boolean[2*n - 1];
         bool[2] = new boolean[2*n - 1];
-        bool[3] = new boolean[2*n - 1];
 
         backtrack(answer,board,bool,0,n);
         return answer;
@@ -31,18 +30,20 @@ class Solution {
         }
 
             for(int j = 0 ; j < board.length ; j++){
-                if(bool[1][j] || bool[2][row+j] || bool[3][(board.length -1) - row +j]) continue;
-                bool[0][row] = true ;
-                bool[1][j] = true ;
-                bool[2][row+j] = true ;
-                bool[3][(board.length -1) - row +j] = true ;
+                if(bool[0][j] || bool[1][row+j] || bool[2][(board.length -1) - row +j]) continue;
+
+                bool[0][j] = true ;
+                bool[1][row+j] = true ;
+                bool[2][(board.length -1) - row +j] = true ;
                 board[row][j] = 'Q';
+
                 backtrack(answer,board,bool,row + 1,n-1);
+
+                bool[0][j] = false ;
+                bool[1][row+j] = false ;
+                bool[2][(board.length -1) - row +j] = false ;
                 board[row][j] = '.';
-                bool[0][row] = false ;
-                bool[1][j] = false ;
-                bool[2][row+j] = false ;
-                bool[3][(board.length -1) - row +j] = false ;
+
 
             }
 
