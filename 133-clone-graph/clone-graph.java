1541491a1381d22e6a4b7b101ba8760412b7aa1c
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-10-24 11:11:04 +0300

    Time: 28 ms (22.49%) | Memory: 41.9 MB (50.38%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -1,45 +1,30 @@
 class Solution {
     public Node cloneGraph(Node node) {
-    if (node == null) return null;
+        Map<Integer,Node> map = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        while(q.peek() != null){
 
-    // **new node to return
-    // MUST BE DEFINED OUTSIDE SO THAT IT CAN BE RETURNED
-    Node newNode = new Node(node.val, new ArrayList<>());
-    // store visited nodes
-    // key is the ORIGINAL node, value is the NEW node
-    HashMap<Node, Node> map = new HashMap();
-    // add starting node to HashMap
-    map.put(node, newNode);
+            Node queueCurr = q.poll();
+            int queueCurrVal = queueCurr.val;
 
-    // store ORIGINAL nodes to be visited in a queue
-    Queue<Node> queue = new LinkedList();
-    // add the start node to the queue
-    queue.add(node);
-    //if more nodes need to be visited
-    while (!queue.isEmpty()) {
-        //search neighbors of top node in the queue
-        Node polledOrigNode = queue.poll();
+            if(!map.containsKey(queueCurrVal))
+                map.put(queueCurrVal, new Node(queueCurrVal));
 
-        // if node 1 has neighbors 2 and 3
-        for (Node origNeighbor : polledOrigNode.neighbors) {
-            // if the map does not contain the neighbor that means
-            // the node needs to be cloned and added to map
-            // check if map contains 2 then 3
-            if (!map.containsKey(origNeighbor)) {
-                // if map does not contain 2 or 3 create a clone 2c, 3c
-                Node newNeighborNode = new Node(origNeighbor.val, new ArrayList<>());
-                // add 2,2c to map and 3,3c
-                map.put(origNeighbor, newNeighborNode);
-                // add 2,3 to queue
-                queue.add(origNeighbor);
-            }
-            // get the cloned node of the original node
-            // add the new neighbor to the cloned nodes neighbor list
-            // get 1c from map to its neighbors, add 2c and 3c
-            map.get(polledOrigNode).neighbors.add(map.get(origNeighbor));
+            Node mapCurr = map.get(queueCurrVal);
+                if(mapCurr.neighbors.isEmpty()){
+                    for(Node neighbor : queueCurr.neighbors){
+                        int neighborVal = neighbor.val;
+                        if(!map.containsKey(neighborVal))
+                            map.put(neighborVal,new Node(neighborVal));
+                        q.add(neighbor);
+                        mapCurr.neighbors.add(map.get(neighborVal));
+
+                    }
+                }
+
         }
-    }
 
-    return newNode;
-}
+        return map.get(1);
+    }
 }
