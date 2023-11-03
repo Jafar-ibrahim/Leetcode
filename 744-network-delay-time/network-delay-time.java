a5edfe64d221e251d7c713e0676400e7deadc101
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-11-03 20:28:58 +0300

    Time: 20 ms (42.62%) | Memory: 46.1 MB (78.97%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,45 +1,31 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        
-        Map<Integer, Map<Integer,Integer>> map = new HashMap<>();
-        for(int[] time : times){
-            map.putIfAbsent(time[0], new HashMap<>());
-            map.get(time[0]).put(time[1], time[2]);
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] edge: times) {
+            graph.putIfAbsent(edge[0], new ArrayList<>());
+            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
         }
-
-        boolean[] visited = new boolean[n+1];
-
-        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
-
-
-
-        pq.add(new int[]{0,k});
-
-        int answer = -1 ;
-
-        while (!pq.isEmpty()){
-
-            int[] pair = pq.poll();
-            int currNode =pair[1];
-            int currTime = pair[0];
-
-            Map<Integer,Integer> neighbors = map.get(currNode);
-
-            if(visited[currNode]) continue;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        boolean[] visited = new boolean[n + 1];
+        int[] minDis = new int[n + 1];
+        Arrays.fill(minDis, Integer.MAX_VALUE);
+        minDis[k] = 0;
+        pq.offer(new int[]{0, k});
+        int max = 0;
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int currNode = curr[1];
+            if (visited[currNode]) continue;
             visited[currNode] = true;
+            int currDis = curr[0];
+            max = currDis;
             n--;
-            answer = currTime;
-
-            if (neighbors != null)
-                for (Map.Entry<Integer,Integer> entry : neighbors.entrySet() ){
-                    int travelTime = entry.getValue();
-                    int neighbor = entry.getKey();
-                    int newTime = currTime + travelTime;
-                    
-                    pq.add(new int[]{newTime,neighbor});
-                }
+            if (!graph.containsKey(currNode)) continue;
+            for (int[] next : graph.get(currNode)) {
+                if (!visited[next[0]] && currDis + next[1] < minDis[next[0]])
+                    pq.offer(new int[]{currDis + next[1], next[0]});
             }
-
-        return n == 0 ? answer : -1;
+        }
+        return n == 0 ? max : -1;
     }
 }
