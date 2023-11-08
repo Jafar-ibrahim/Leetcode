7000d7b0ceb09b924a9b620e3849edcec0ffe776
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-11-08 15:18:10 +0300

    Time: 10 ms (99.24%) | Memory: 55.7 MB (76.37%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,49 +1,28 @@
 class Solution {
-    class State {
-	int node;
-	double prob;
-	State(int _node, double _prob) {
-		node = _node;
-		prob = _prob;
-	}
-}
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        double[] maxProb = new double[n];
+        maxProb[start] = 1.0;
 
-public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        for (int i = 0; i < n - 1; i++) {
+            boolean hasUpdate = false;
+            for (int j = 0; j < edges.length; j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double pathProb = succProb[j];
+                if (maxProb[u] * pathProb > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * pathProb;
+                    hasUpdate = true;
+                }
+                if (maxProb[v] * pathProb > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * pathProb;
+                    hasUpdate = true;
+                }
+            }
+            if (!hasUpdate) {
+                break;
+            }
+        }
 
-	// build graph -> double[0]: node, double[1]: parent-to-node prob
-	Map<Integer, List<double[]>> map = new HashMap<>();
-	for (int i = 0; i < edges.length; ++i) {
-		int[] edge = edges[i];
-
-		map.putIfAbsent(edge[0], new ArrayList<>());
-		map.putIfAbsent(edge[1], new ArrayList<>());
-
-		map.get(edge[0]).add(new double[] {edge[1], succProb[i]});
-		map.get(edge[1]).add(new double[] {edge[0], succProb[i]});
-	}
-
-	double[] probs = new double[n];  // best prob so far for each node
-	PriorityQueue<State> pq = new PriorityQueue<>((a, b) -> (((Double) b.prob).compareTo((Double) a.prob)));
-	pq.add(new State(start, 1.0));
-
-	while (!pq.isEmpty()) {
-
-		State state = pq.poll();
-		int parent = state.node;
-		double prob = state.prob;
-
-		if (parent == end) return prob;
-
-		for (double[] child : map.getOrDefault(parent, new ArrayList<>())) {
-			// add to pq only if: it can make a better prob
-			if (probs[(int) child[0]] >= prob * child[1]) continue;
-
-			pq.add(new State((int) child[0], prob * child[1]));
-			probs[(int) child[0]] = prob * child[1];
-		}
-
-	}
-
-	return 0;
-}
+        return maxProb[end];
+    }
 }
